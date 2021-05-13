c02ea15c7b70bb08679e1241e0e660f38661c99b
Nyein Chan Aung <ninja.aung02@gmail.com>
2021-05-13 04:12:42 -0700

    Updated app.js for offline testing

--- app.js
+++ app.js
@@ -6,15 +6,21 @@ const express = require('express')
 const app = express()
 const AWS = require('aws-sdk');
 
-
 const USERS_TABLE = process.env.USERS_TABLE;
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const IS_OFFLINE = process.env.IS_OFFLINE;
+let dynamoDb;
+if (IS_OFFLINE === 'true') {
+  dynamoDb = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000'
+  })
+  console.log(dynamoDb);
+} else {
+  dynamoDb = new AWS.DynamoDB.DocumentClient();
+};
 
 app.use(bodyParser.json({ strict: false }));
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
 
 // Get User endpoint
 app.get('/users/:userId', function (req, res) {
